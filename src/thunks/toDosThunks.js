e98c166b05a8501bd0a2f8bdb37363b04434ab72
Maria del Mar Cegarra <maria.cegarra2303@gmail.com>
2022-05-11 00:14:14 +0200

    Add todo with api thunks uwu

--- src/thunks/toDosThunks.js
+++ src/thunks/toDosThunks.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 import {
+  addtoDoActionCreator,
   deleteToDoActionCreator,
+  editToDoActionCreator,
   loadToDosActionCreator,
 } from "../redux/features/toDos/toDosSlice";
 
@@ -13,5 +15,19 @@ export const deleteToDoThunk = (id) => async (dispatch) => {
   const { status } = await axios.delete(process.env.REACT_APP_API_URL + id);
   if (status === 200) {
     dispatch(deleteToDoActionCreator(id));
+  }
+};
+
+export const updateToDoThunk = (id, todo) => async (dispatch) => {
+  const { status } = await axios.put(process.env.REACT_APP_API_URL + id, todo);
+  if (status === 200) {
+    dispatch(editToDoActionCreator({ id, todo }));
+  }
+};
+
+export const addToDoThunk = (toDo) => async (dispatch) => {
+  const { status } = await axios.post(process.env.REACT_APP_API_URL, toDo);
+  if (status === 200) {
+    dispatch(addtoDoActionCreator(toDo));
   }
 };
